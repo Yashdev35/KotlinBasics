e2bb552901caa02f16b3e3ef644fa1452c6747b4
Yash VIjay Kumbhkarn <cse230001083@iiti.ac.in>
2024-01-25 23:23:50 +0530

    idk

---
+++ app/src/main/java/com/example/kotlinbasics/Checking triple equal to.kt
@@ -0,0 +1,14 @@
+package com.example.kotlinbasics
+
+fun main(){
+    checking()
+}
+fun checking(){
+    val name = "89"
+    val age = "89"
+    if(name === age){
+        println("kaam nhi karta hai")
+    }else {
+        println("kaam karta hai")
+    }
+}
